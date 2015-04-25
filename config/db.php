7ef64ff4f3c19148345c42ac336669d3831dcf57
milezoom <milezoom@muslims-notebook>
2015-04-26 06:50:54 +0700

    update db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2minimal',
-    'username' => 'root',
+    'dsn' => 'pgsql:host=localhost;dbname=test',
+    'username' => 'muslim',
     'password' => '',
     'charset' => 'utf8',
 ];
