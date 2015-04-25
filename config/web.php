eb844a849cf1b4d57ca73b1cf3c815d1b4a60ab6
milezoom <milezoom@muslims-notebook>
2015-04-25 10:31:58 +0700

    update prettyURL

--- config/web.php
+++ config/web.php
@@ -7,7 +7,11 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
-            'request' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptsName' => false,
+        ],
+        'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'hLITDK9lh4O7nr8mDwSlItRDiolOftpx',
         ],
