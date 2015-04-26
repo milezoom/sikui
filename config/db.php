c931ba938f6af99c1a68baa8d01c88d310937a48
milezoom <milezoom@muslims-notebook>
2015-04-26 07:04:30 +0700

    benerin project abis nambahin gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=test',
-    'username' => 'muslim',
-    'password' => '',
-    'charset' => 'utf8',
-];
