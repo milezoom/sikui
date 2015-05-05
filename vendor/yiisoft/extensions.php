7b2b8c82b7906a663eda7097ccebbdec5c3645e0
milezoom <milezoom@muslims-notebook>
2015-05-05 16:26:43 +0700

    add extension for pdf creation

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'kartik-v/yii2-mpdf' => 
+  array (
+    'name' => 'kartik-v/yii2-mpdf',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/mpdf' => $vendorDir . '/kartik-v/yii2-mpdf',
+    ),
+  ),
+);
