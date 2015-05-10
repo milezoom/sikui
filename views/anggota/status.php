0a9ce76ab3a954af192402e7714ecd90ff3b299c
utariazzahra <utari.azzahra30@gmail.com>
2015-05-10 16:46:55 +0700

    ubah status anggota

---
+++ views/anggota/status.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Anggota */
+
+$this->title = 'Ubah Status Anggota: ' . ' ' . $model->no_anggota;
+$this->params['breadcrumbs'][] = ['label' => 'Anggotas', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->no_anggota, 'url' => ['view', 'id' => $model->no_anggota]];
+$this->params['breadcrumbs'][] = 'Status';
+?>
+<div class="anggota-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
