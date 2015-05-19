c7b59cfaed31f64680d357c0c5d009757829f5e1
husimaz <hussein.azhar@gmail.com>
2015-05-19 14:28:21 +0700

    update update dikit

--- views/transaksi-pinjaman/_formBarang.php
+++ views/transaksi-pinjaman/_formBarang.php
@@ -18,31 +18,29 @@ use app\models\Anggota;
 
     <?= $form->field($model, 'kode_trans')->textInput(['maxlength' => 10]) ?>
 
-    <?= $form->field($model, 'kode_pinjaman')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'kode_pinjaman')->textInput(['value'=> 'PJBG', 'readonly' => true]) ?>
 
    <?= $form->field($model, 'no_anggota')->dropDownList(
 		ArrayHelper::map(Anggota::find()->all(),'no_anggota','nama'),
 		['prompt'=>'Select Anggota']
+	) ?>
+	
+	<?= $form->field($model, 'kode_barang')->dropDownList(
+		ArrayHelper::map(Barang::find()->all(),'kode','nama'),
+		['prompt'=>'Select Barang']
 	) ?>
 
     <?= $form->field($model, 'jumlah')->textInput() ?>
 
     <?= $form->field($model, 'sisa_piutang')->textInput() ?>
 
-    <?= $form->field($model, 'tgl_pinjam')->textInput() ?>
+    <?= $form->field($model, 'tgl_pinjam')->textInput(['type' => 'date']) ?>
 
-    <?= $form->field($model, 'jatuh_tempo')->textInput() ?>
-
-    <?= $form->field($model, 'banyak_angsuran')->textInput() ?>
-
-    <?= $form->field($model, 'denda')->textInput() ?>
-
-    <?= $form->field($model, 'kode_barang')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'jatuh_tempo')->textInput(['type' => 'date']) ?>    
 	
-	<?= $form->field($model, 'kode_barang')->dropDownList(
-		ArrayHelper::map(Barang::find()->all(),'kode','nama'),
-		['prompt'=>'Select Barang']
-	) ?>
+	<?= $form->field($model, 'banyak_angsuran')->radioList(array('5' => '5 kali','10' => '10 kali', '15' => '15 kali'))?> 
+
+    <?= $form->field($model, 'denda')->textInput(['value'=> 0]) ?>
 	
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
