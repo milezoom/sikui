a153111aa5e1b10b7ef1d4cf58873687aa89eb7d
utariazzahra <utari.azzahra30@gmail.com>
2015-05-25 20:39:10 +0700

    FORM BARU!

--- views/anggota/_form.php
+++ views/anggota/_form.php
@@ -18,42 +18,28 @@ use app\models\Unit;
 	
     <?= $form->field($model, 'no_anggota')->textInput(['maxlength' => 20, 'placeholder' => 'Masukan 10 digit nomor anggota'])->label('Nomor Anggota *') ?>
 
-    <?= $form->field($model, 'nama')->textInput(['maxlength' => 30, 'placeholder' => 'Nama Anggota'])->label('Nomor Anggota *') ?>
-
-<<<<<<< HEAD
-    <?= $form->field($model, 'kode_unit')->dropDownList(['0000000001'=> 'Fasilkom', '0000000002' => 'Rektorat'])->label('Kode Unit atau Instansi *') ?>
+    <?= $form->field($model, 'nama')->textInput(['maxlength' => 30, 'placeholder' => 'Nama Anggota'])->label('Nama Anggota *') ?>
 
     <?= $form->field($model, 'alamat')->textArea(['maxlength' => 150, 'placeholder' => 'Alamat saat ini'])->label('Alamat *') ?>
-=======
-    <?= $form->field($model, 'nama')->textInput(['maxlength' => 30, 'placeholder' => 'Nama Anggota']) ?>
-	
+
     <?= $form->field($model, 'kode_unit')->dropDownList(
 		ArrayHelper::map(Unit::find()->all(),'kode','nama'),
 		['prompt'=>'Pilih Unit']
 	) ?>
->>>>>>> origin/master
-
+	
     <?= $form->field($model, 'tgl_lahir')->textInput(['type' => 'date'])->label('Tanggal Lahir *') ?>
 
     <?= $form->field($model, 'no_telepon')->textInput(['maxlength' => 15, 'placeholder' => 'No Telp/HP']) ?>
 
     <?= $form->field($model, 'jenis_kelamin')->dropDownList(['Perempuan' => 'Perempuan','Laki Laki' => 'Laki-laki'])->label('Jenis Kelamin *')?>
 
-<<<<<<< HEAD
     <?= $form->field($model, 'thn_pensiun')->textInput()->label('Tahun Pensiun *') ?>
-=======
-    <?= $form->field($model, 'thn_pensiun')->textInput() ?>
->>>>>>> origin/master
 
     <?= $form->field($model, 'status')->dropDownList(['non' => 'Tidak Aktif','aktif' => 'Aktif'])->label('Status Keanggotaan *')?>
 
-<<<<<<< HEAD
-    <?= $form->field($model, 'is_pns')->dropDownList(['Honorer' => 'Honorer','PNS' => 'PNS'])->label('Status Kepegawaian *')?>
-=======
-    <?= $form->field($model, 'is_pns')->radioList(array('0' => 'Honorer','1' => 'PNS'))?>
+    <?= $form->field($model, 'is_pns')->radioList(['P-UI' => 'Pegawai UI','PNS' => 'PNS'])->label('Status Kepegawaian *')?>
     
     <?= $form->field($user, 'role')->radioList(array('anggota' => 'Anggota Biasa','admin' => 'Admin Sistem')) ?>
->>>>>>> origin/master
 
     <?= $form->field($model, 'no_ktp')->textInput(['maxlength' => 16, 'placeholder' => 'Masukan 16 digit Nomor KTP']) ?>
 
